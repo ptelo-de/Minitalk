9ce3c7aeab2c654b5da503ccbac58b85da2405f2
Pilar Telo De Lima Homem Da Trindade <ptelo-de@c1r6s6.42lisboa.com>
2024-08-24 14:46:50 +0100

    is printing that received each signal properly

--- client.c
+++ client.c
@@ -1,23 +1,12 @@
 //getppid() returns the process ID of the parent of the calling process. 
 #include "minitalk.h"
-void    ft_error(void)
+void    ft_error(char *s)
 {
-    write (2, "Error\n", 7);
+    ft_putendl_fd(s,2);
     exit(1); //return 1 or -1?
 }
 
-// void    ft_sendbit(int pid, int wc, char **av)
-// {
-    
-//  while(*av)
-//  {
-//      while(**av)
-//      {
 
-//      }
-//      av++;
-//  }
-// }
 //calculates the number an atribute can be translated
 //does not allow buffer overflow or returning the number 
 // if trash values follow the digit part.
@@ -30,7 +19,7 @@ int ft_atoi3(const char *nptr,  int i)
 
     nb = 0;
     if (!nptr[0])
-        ft_error();
+        ft_error("empty string in atoi input");
     while (((nptr[i] >= 9 && nptr[i] <= 13) || nptr[i] == 32))
         i++;
     if (nptr[i] == '+' && nptr[i + 1] != 0)
@@ -38,12 +27,12 @@ int ft_atoi3(const char *nptr,  int i)
     while (nptr[i] <= '9' && nptr[i] >= '0')
     {
         if (nb * 10 < nb)
-            ft_error();
+            ft_error("overflow in atoi input");
         nb = 10 * nb + (nptr[i] - 48);
         i++;
     }
     if (nptr[i] || nb > 2147483647 || nb < -2147483648)
-        ft_error();
+        ft_error("n atoi input, integer overflow or wrong chars");
     return (nb);
 }
 void    ft_sendlen(int pid, int len)
@@ -57,10 +46,13 @@ void    ft_sendlen(int pid, int len)
         if(len & 1)
             check_kill = kill(pid, SIGUSR1);
         else
+        {
+            ft_printf("estou aqui\n");
             check_kill = kill(pid, SIGUSR2);
+        }
         if(check_kill == -1)
-            ft_error();
-        len >> 1;
+            ft_error("kill function returned an error while sending signal to server");
+        len = len >> 1;
         usleep(2000); //in microseconds 10^⁽⁻6) sec
     }
 }
@@ -72,5 +64,6 @@ int main(int ac, char **av)
         ft_sendlen(ft_atoi3(av[1], 0), ft_strlen(av[2]));
         
     }
-    ft_error();
+    else 
+        ft_error("Too many or too little arguments");
 }
--- server.c
+++ server.c
@@ -1,3 +1,4 @@
+
 #include "minitalk.h"
 //getppid() returns the process ID of the parent of the calling process.
 void handle_sigusr1(int sig, siginfo_t *info, void *context) {
@@ -5,24 +6,34 @@ void handle_sigusr1(int sig, siginfo_t *info, void *context) {
     static int pid_client;
     pid_client = info->si_pid;
     (void)pid_client;
-    ft_printf("Received SIGUSR1 signal with number: %d\n", sig);
+    if (sig == SIGUSR1)
+        ft_printf("Received SIGUSR1 signal with number: %d\n", sig);
+    if (sig == SIGUSR2)
+        ft_printf("Received SIGUSR2 signal with number: %d\n", sig);
 }
 int main(void)
 {
     struct sigaction sa;
+
+    ft_printf("PID: %d\n", getpid());
+    sigemptyset(&sa.sa_mask);
     sa.sa_sigaction = handle_sigusr1;  // Specify the handler function
     sa.sa_flags = 0;                 // No special flags
-    sigemptyset(&sa.sa_mask);
-    if (sigaction(SIGUSR1, &sa, NULL) == -1) {
+    if (sigaction(SIGUSR1, &sa, NULL) == -1)
+    {
         ft_printf("sigaction\n");
         exit(1);
     }
-     ft_printf("PID: %d\n", getpid());
+    if (sigaction(SIGUSR2, &sa, NULL) == -1)
+    {
+        ft_printf("sigaction\n");
+        exit(1);
+    }
     while(1)
     {
         pause();
     }
-
+    return(0);
 }
 //ler 32 bits, while i <= 32 salva no len.
 //allocar s com len array
